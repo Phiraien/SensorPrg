b5930a15bb7036f665b95f48f5c817579aa3c399
Phiraien <kerala.emperor2@gmail.com>
2026-07-29 12:06:09 +0530

    Update pir.c

--- PIR Motion Sensor/pir.c
+++ PIR Motion Sensor/pir.c
@@ -1,10 +1,10 @@
 void setup() {
-  pinMode(2, INPUT);
+  pinMode(8, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  int motion = digitalRead(2);
+  int motion = digitalRead(8);
   Serial.println(motion);
   delay(500);
 }
