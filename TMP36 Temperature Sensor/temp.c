9ea3e65a7af57b957cb855c5010856417c424c64
Phiraien <kerala.emperor2@gmail.com>
2026-07-29 12:00:22 +0530

    Create temp.c

---
+++ TMP36 Temperature Sensor/temp.c
@@ -0,0 +1,26 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int sensorValue=analogRead(A0);
+  float voltage=sensorValue*(5.0/1023.0);
+  float temperature=(voltage-0.5)*100;
+  Serial.print("Temperature:");
+  Serial.print(temperature);
+  Serial.println("C");
+}
+
+
+
+
+
+
+
+
+
+
